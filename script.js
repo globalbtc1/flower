f6e79b02902e812a2b7519c4f6ba2f3e08bc05d5
zer0 <43705798+globalbtc1@users.noreply.github.com>
2026-07-22 21:56:07 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('playBtn').addEventListener('click', function() {
-    document.getElementById('welcomeScreen').style.display = 'none';
-    document.getElementById('mainScreen').style.display = 'block';
-    
-    let music = document.getElementById('bgMusic');
-    music.volume = 1.0; 
-    music.play();
-});
